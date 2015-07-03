8cf2014684d1aa6c0e6e16b854b1dc900029a6e0
Jared Sturdy <sturdy@cern.ch>
2015-07-03 16:36:15 +0200

    GEMHwDevice logger working, updated HW device interfaces to remove the passing of a logger object, fixed the implementations in gemHwMonitorWeb but otherwise did not modify gemHwMonitor sub package

--- gemHwMonitor/include/gem/hwMonitor/gemHwMonitorBase.h
+++ gemHwMonitor/include/gem/hwMonitor/gemHwMonitorBase.h
@@ -8,125 +8,114 @@
 #include "xdaq/Application.h"
 #include "xgi/framework/Method.h"
 
-#include "gem/utils/GEMLogging.h"
 #include "gem/utils/gemXMLparser.h"
 #include "gem/utils/gemComplexDeviceProperties.h"
 #include "gem/utils/gemDeviceProperties.h"
 
 namespace gem {
-  namespace hwMonitor {
-    template <class T>
-      class gemHwMonitorBase
-      {
-      public:
-	gemHwMonitorBase()
-	  throw (xdaq::exception::Exception) :
-	gemLogger_(log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("gemHwMonitorLogger")))
-	  {
-	    gemDevice_ = new T();
-	    //gemLogger_.setLogLevel(DEBUG_LOG_LEVEL);
-	  }
-	
-	virtual ~gemHwMonitorBase()
-	  {
-	    delete gemDevice_;
-	  }
+    namespace hwMonitor {
+        template <class T>
+        class gemHwMonitorBase
+        {
+            public:
+                gemHwMonitorBase()
+                    throw (xdaq::exception::Exception)
+                {
+                    gemDevice_ = new T();
+                }
 
-	bool isConfigured()
-	  throw (xgi::exception::Exception)
-	{return isConfigured_;}
+                virtual ~gemHwMonitorBase()
+                {
+                    delete gemDevice_;
+                }
 
-	void setIsConfigured(bool state)
-	//throw (xgi::exception::Exception)
-	{
-	  INFO("isConfigured_(" << isConfigured_ << ")");
-	  INFO("setIsConfigured(state = " << state << ")");
-	  isConfigured_=state;
-	  INFO("isConfigured_(" << isConfigured_ << ") = state");
-	}
+                bool isConfigured()
+                    throw (xgi::exception::Exception)
+                {return isConfigured_;}
 
-	const std::string getDeviceId()
-	  throw (xgi::exception::Exception);
+                void setIsConfigured(bool state)
+                    throw (xgi::exception::Exception)
+                {isConfigured_=state;}
 
-	/**
-	 *   Get subdevice status
-	 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
-	 */
-	unsigned int getSubDeviceStatus (unsigned int i)
-	//throw (xgi::exception::Exception)
-	{return subDeviceStatus_.at(i);}
+                const std::string getDeviceId()
+                    throw (xgi::exception::Exception);
 
-	/**
-	 *   Set subdevice status
-	 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
-	 */
-	void setSubDeviceStatus (const unsigned int deviceStatus, const unsigned int i)
-	  throw (xgi::exception::Exception)
-	{subDeviceStatus_.at(i) = deviceStatus;}
+                /**
+                 *   Get subdevice status
+                 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
+                 */
+                unsigned int getSubDeviceStatus (unsigned int i)
+                    throw (xgi::exception::Exception)
+		            {return subDeviceStatus_.at(i);}
 
-	/**
-	 *   Add subdevice status
-	 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
-	 */
-	void addSubDeviceStatus (unsigned int deviceStatus)
-	  throw (xgi::exception::Exception)
-	{subDeviceStatus_.push_back(deviceStatus);}
+                /**
+                 *   Set subdevice status
+                 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
+                 */
+                void setSubDeviceStatus (const unsigned int deviceStatus, const unsigned int i)
+                    throw (xgi::exception::Exception)
+		            {subDeviceStatus_.at(i) = deviceStatus;}
 
-	/**
-	 *   Get device status
-	 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
-	 */
-	unsigned int getDeviceStatus ()
-	  throw (xgi::exception::Exception)
-	{return deviceStatus_;}
+                /**
+                 *   Add subdevice status
+                 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
+                 */
+                void addSubDeviceStatus (unsigned int deviceStatus)
+                    throw (xgi::exception::Exception)
+		            {subDeviceStatus_.push_back(deviceStatus);}
 
-	/**
-	 *   Set device status
-	 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
-	 */
-	void setDeviceStatus (const unsigned int deviceStatus)
-	  throw (xgi::exception::Exception)
-	{deviceStatus_ = deviceStatus;}
-	/**
-	 *   Set device configuration
-	 */
-	void setDeviceConfiguration(T& device)
-	  throw (xgi::exception::Exception);
-	/**
-	 *   Get device reference
-	 */
-	T*  getDevice()
-	  throw (xgi::exception::Exception)
-	  {return gemDevice_;}
+                /**
+                 *   Get device status
+                 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
+                 */
+                unsigned int getDeviceStatus ()
+                    throw (xgi::exception::Exception)
+		            {return deviceStatus_;}
 
-	int getNumberOfSubDevices()
-	  throw (xgi::exception::Exception);
+                /**
+                 *   Set device status
+                 *   0 - device is working well, 1 - device has errors, 2 - device status unknown
+                 */
+                void setDeviceStatus (const unsigned int deviceStatus)
+                    throw (xgi::exception::Exception)
+		            {deviceStatus_ = deviceStatus;}
+                /**
+                 *   Set device configuration
+                 */
+                void setDeviceConfiguration(T& device)
+                    throw (xgi::exception::Exception);
+                /**
+                 *   Get device reference
+                 */
+                T*  getDevice()
+                    throw (xgi::exception::Exception)
+                    {return gemDevice_;}
 
-	const std::string getCurrentSubDeviceId(unsigned int subDeviceNumber)
-	  throw (xgi::exception::Exception);
+                int getNumberOfSubDevices()
+                    throw (xgi::exception::Exception);
 
-	/**
-	 *   Access to board utils
-	 virtual void boardUtils ()
-	 throw (xgi::exception::Exception);
-	*/
-      protected:
-      private:
-	bool isConfigured_;
-	unsigned int deviceStatus_; // 0 - device is working well, 1 - device has errors, 2 - device status unknown
-	std::vector<unsigned int> subDeviceStatus_; // 0 - device is working well, 1 - device has errors, 2 - device status unknown
+                const std::string getCurrentSubDeviceId(unsigned int subDeviceNumber)
+                                    throw (xgi::exception::Exception);
 
-	log4cplus::Logger gemLogger_;
+                /**
+                 *   Access to board utils
+                virtual void boardUtils ()
+                    throw (xgi::exception::Exception);
+                 */
+            protected:
+            private:
+                bool isConfigured_;
+                unsigned int deviceStatus_; // 0 - device is working well, 1 - device has errors, 2 - device status unknown
+                std::vector<unsigned int> subDeviceStatus_; // 0 - device is working well, 1 - device has errors, 2 - device status unknown
+                std::string xmlConfigFileName_;
+                T* gemDevice_;
+        };
 
-	std::string xmlConfigFileName_;
-	T* gemDevice_;
-      };
-
-    typedef gemHwMonitorBase<gem::utils::gemSystemProperties> gemHwMonitorSystem;
-    typedef gemHwMonitorBase<gem::utils::gemCrateProperties> gemHwMonitorCrate;
-    typedef gemHwMonitorBase<gem::utils::gemGLIBProperties> gemHwMonitorGLIB;
-    typedef gemHwMonitorBase<gem::utils::gemOHProperties> gemHwMonitorOH;
-    typedef gemHwMonitorBase<gem::utils::gemVFATProperties> gemHwMonitorVFAT;
-  } // end namespace hwMon
+        typedef gemHwMonitorBase<gem::utils::gemSystemProperties> gemHwMonitorSystem;
+        typedef gemHwMonitorBase<gem::utils::gemCrateProperties> gemHwMonitorCrate;
+        typedef gemHwMonitorBase<gem::utils::gemGLIBProperties> gemHwMonitorGLIB;
+        typedef gemHwMonitorBase<gem::utils::gemOHProperties> gemHwMonitorOH;
+        typedef gemHwMonitorBase<gem::utils::gemVFATProperties> gemHwMonitorVFAT;
+    } // end namespace hwMon
 } // end namespace gem
 #endif
--- gemHwMonitor/include/gem/hwMonitor/gemHwMonitorHelper.h
+++ gemHwMonitor/include/gem/hwMonitor/gemHwMonitorHelper.h
@@ -10,77 +10,60 @@
 #include "gem/utils/gemDeviceProperties.h"
 
 #include "gemHwMonitorBase.h"
-#include "gem/utils/GEMLogging.h"
 
 namespace gem {
-  namespace hwMonitor {
-    class gemHwMonitorHelper
-    {
-    public:
-      gemHwMonitorHelper(log4cplus::Logger const& gemLogger, gemHwMonitorSystem* gemSystem) 
-	throw (xdaq::exception::Exception) :
-      ptr_gemSystem_(gemSystem),
-	gemLogger_(gemLogger)
-	{
-	  //gemLogger_.setLogLevel(DEBUG_LOG_LEVEL);
-	  WARN("gemHwMonitorHelper::getting env vars");
-	  std::string defaulXMLcfgFile = std::getenv("BUILD_HOME");
-	  WARN("gemHwMonitorHelper::setting default file");
-	  defaulXMLcfgFile +="/gemdaq-testing/gembase/xml/gem_conf_proposal.xml";
-	  WARN("gemHwMonitorHelper::setting file");
-	  this->setXMLconfigFile(defaulXMLcfgFile.c_str());
-	  WARN("gemHwMonitorHelper::setting gemsystem pointer");
-	  //ptr_gemSystem_ = gemSystem;
-	  WARN("gemHwMonitorHelper::setting gemsystem pointer device status");
-	  ptr_gemSystem_->setDeviceStatus(2);
-	  WARN("gemHwMonitorHelper::constructor finished");
-	}
+    namespace hwMonitor {
+        class gemHwMonitorHelper
+        {
+            public:
+                gemHwMonitorHelper(gemHwMonitorSystem* gemSystem)
+                    throw (xdaq::exception::Exception)
+                {
+                    std::string defaulXMLcfgFile = std::getenv("BUILD_HOME");
+                    defaulXMLcfgFile +="/gemdaq-testing/gembase/xml/gem_conf_proposal.xml";
+                    this->setXMLconfigFile(defaulXMLcfgFile.c_str());
+                    ptr_gemSystem_ = gemSystem;
+                    ptr_gemSystem_->setDeviceStatus(2);
+                }
 
-      virtual ~gemHwMonitorHelper()
-	{
-	  delete gemXMLparser_;
-	}
+                virtual ~gemHwMonitorHelper()
+                {
+                    delete gemXMLparser_;
+                }
 
-      void setXMLconfigFile (const char* inputXMLfilename)
-      //throw (xgi::exception::Exception)
-      {
-	WARN("gemHwMonitorHelper::setXMLconfigFile");
-	xmlConfigFileName_ = inputXMLfilename;
-	if (ptr_gemSystem_) {
-	  WARN("gemHwMonitorHelper::device state changed");
-	  //something is happening here, but what???
-	  ptr_gemSystem_->setIsConfigured(false);
-	  return;
-	} else {
-	  WARN("gemHwMonitorHelper::New XML file set, but device state hasn't changed (NULL-pointer)");
-	  return;
-	}
-      }
-      const std::string getXMLconfigFile ()
-	throw (xgi::exception::Exception)
-      {
-	return xmlConfigFileName_;
-      }
-      void setDBSconfigFile (std::string inputDBSfilename)
-	throw (xgi::exception::Exception){}
-      const std::string getDBSconfigFile ()
-	throw (xgi::exception::Exception){return "Not implemented yet";}
-      void configure()
-	throw (xgi::exception::Exception)
-      {
-	gemXMLparser_ = new gem::utils::gemXMLparser(xmlConfigFileName_);
-	gemXMLparser_->parseXMLFile();
-	ptr_gemSystem_->setDeviceConfiguration(*(gemXMLparser_->getGEMDevice()));
-      }
+                void setXMLconfigFile (const char* inputXMLfilename)
+                    throw (xgi::exception::Exception)
+                {
+                    xmlConfigFileName_ = inputXMLfilename;
+                    if (ptr_gemSystem_) 
+                        ptr_gemSystem_->setIsConfigured(false);
+                    else 
+                        std::cout << "[WARNING] New XML file set, but device state hasn't changed (NULL-pointer)" << std::endl;
+                }
+                const std::string getXMLconfigFile ()
+                    throw (xgi::exception::Exception)
+                {
+                    return xmlConfigFileName_;
+                }
+                void setDBSconfigFile (std::string inputDBSfilename)
+                    throw (xgi::exception::Exception){}
+                const std::string getDBSconfigFile ()
+                    throw (xgi::exception::Exception){return "Not implemented yet";}
+                void configure()
+                    throw (xgi::exception::Exception)
+                {
+                    gemXMLparser_ = new gem::utils::gemXMLparser(xmlConfigFileName_);
+                    gemXMLparser_->parseXMLFile();
+                    ptr_gemSystem_->setDeviceConfiguration(*(gemXMLparser_->getGEMDevice()));
+                }
 
-    protected:
-    private:
-      gemHwMonitorSystem* ptr_gemSystem_;
-      gem::utils::gemXMLparser* gemXMLparser_;
-      std::string xmlConfigFileName_;
-      log4cplus::Logger gemLogger_;
+            protected:
+            private:
+                gemHwMonitorSystem* ptr_gemSystem_;
+                gem::utils::gemXMLparser* gemXMLparser_;
+                std::string xmlConfigFileName_;
 
-    };
-  }
+        };
+    }
 }
 #endif
