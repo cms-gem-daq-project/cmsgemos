36b11222cc30322b6512349081f3015c7f54940f
Jared Sturdy <sturdy@cern.ch>
2016-05-23 19:36:38 +0200

    testing scans with CTP7

--- gemhardware/include/gem/hw/GEMHwDevice.h
+++ gemhardware/include/gem/hw/GEMHwDevice.h
@@ -59,7 +59,7 @@ namespace gem {
          so it helps to retry a few times in the case of a failure
          that is recognized
       */
-      static const unsigned MAX_IPBUS_RETRIES = 5;
+      static const unsigned MAX_IPBUS_RETRIES = 3;
 
       /**
        * @struct OpticalLinkStatus
