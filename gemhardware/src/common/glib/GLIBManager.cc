1fce8d512884613b046281b1f7bfda4d722d87a0
Jared Sturdy <sturdy@cern.ch>
2016-06-27 14:10:53 +0200

    updated some exceptions in GLIB classes

--- gemhardware/src/common/glib/GLIBManager.cc
+++ gemhardware/src/common/glib/GLIBManager.cc
@@ -253,7 +253,7 @@ void gem::hw::glib::GLIBManager::initializeAction()
         m_glibMonitors.at(slot)->startMonitoring();
       } else {
         ERROR("GLIBManager:: unable to communicate with GLIB in slot " << (slot+1));
-        XCEPT_RAISE(gem::hw::glib::exception::Exception, "initializeAction failed");
+        XCEPT_RAISE(gem::hw::glib::exception::HardwareProblem, "initializeAction failed");
       }
     } catch (uhalException const& ex) {
       ERROR("GLIBManager::caught uHAL exception " << ex.what());
@@ -285,7 +285,7 @@ void gem::hw::glib::GLIBManager::initializeAction()
     } else {
       ERROR("GLIBManager::GLIB in slot " << (slot+1) << " is not connected");
       //fireEvent("Fail");
-      XCEPT_RAISE(gem::hw::glib::exception::Exception, "initializeAction failed");
+      XCEPT_RAISE(gem::hw::glib::exception::HardwareProblem, "initializeAction failed");
       // maybe raise exception so as to not continue with other cards? let's just return for the moment
       return;
     }
@@ -330,7 +330,7 @@ void gem::hw::glib::GLIBManager::configureAction()
     } else {
       ERROR("GLIBManager::GLIB in slot " << (slot+1) << " is not connected");
       //fireEvent("Fail");
-      XCEPT_RAISE(gem::hw::glib::exception::Exception, "configureAction failed");
+      XCEPT_RAISE(gem::hw::glib::exception::HardwareProblem, "configureAction failed");
       // maybe raise exception so as to not continue with other cards?
     }
   }
@@ -361,7 +361,7 @@ void gem::hw::glib::GLIBManager::startAction()
     } else {
       ERROR("GLIB in slot " << (slot+1) << " is not connected");
       //fireEvent("Fail");
-      XCEPT_RAISE(gem::hw::glib::exception::Exception, "startAction failed");
+      XCEPT_RAISE(gem::hw::glib::exception::HardwareProblem, "startAction failed");
       // maybe raise exception so as to not continue with other cards? let's just return for the moment
       return;
     }
