2e59d513f77dfa81ef40ec5b3d46f0fdfb81dcc9
Jared Sturdy <sturdy@cern.ch>
2016-07-29 19:32:26 +0200

    updates to make OH python scripts work with generic firmware

--- setup/scripts/python/optohybrid_board_info_uhal.py
+++ setup/scripts/python/optohybrid_board_info_uhal.py
@@ -52,10 +52,10 @@ if options.testbeam:
         ipaddr        = '137.138.115.185'
 uri           = "chtcp-2.0://localhost:10203?target=%s:50001"%(ipaddr)
 
-address_table = "file://${GEM_ADDRESS_TABLE_PATH}/glib_address_table.xml"
+address_table = "file://${GEM_ADDRESS_TABLE_PATH}/uhal_gem_amc_glib.xml"
 optohybrid    = uhal.getDevice( "optohybrid" , uri, address_table )
 
-address_table = "file://${GEM_ADDRESS_TABLE_PATH}/glib_address_table.xml"
+address_table = "file://${GEM_ADDRESS_TABLE_PATH}/uhal_gem_amc_glib.xml"
 glib          = uhal.getDevice( "glib"       , uri, address_table )
 
 SAMPLE_TIME = 1.
@@ -84,7 +84,7 @@ if options.clkSrc in [0,1,2]:
 #print "-> OH VFATs accessible: 0x%x"%(readRegister(glib,"VFATs_TEST"))
 if options.trgSrc in [0,1,2,3,4]:
         setTriggerSource(optohybrid,options.gtx,options.trgSrc)
-	
+
 if options.localT1:
         #configureLocalT1(optohybrid,options.gtx,0x0,0x0,0,25,100)
         sendL1ACalPulse(optohybrid,options.gtx,15)
@@ -96,42 +96,42 @@ clocking = getClockingInfo(optohybrid,options.gtx)
 #OH:  TrgSrc  SBitSrc  FPGA PLL    EXT PLL    CDCE     GTX  RefCLKSrc
 #->:     0x0      0x0       0x0       0x0      0x1     0x1        0x1
 if options.v2b:
-        print "Sources:  %6s  %7s"%("TrgSrc","SBitSrc")
-        print "             0x%x      0x%x"%(
-                readRegister(optohybrid,"GLIB.OptoHybrid_%d.OptoHybrid.CONTROL.TRIGGER.SOURCE"%(options.gtx)),
-                readRegister(optohybrid,"GLIB.OptoHybrid_%d.OptoHybrid.CONTROL.HDMI_OUTPUT.SBITS"%(options.gtx)))
+    print "Sources:  %6s  %7s"%("TrgSrc","SBitSrc")
+    print "             0x%x      0x%x"%(
+            readRegister(optohybrid,"GEM_AMC.OH.OH%d.CONTROL.TRIGGER.SOURCE"%(options.gtx)),
+            readRegister(optohybrid,"GEM_AMC.OH.OH%d.CONTROL.HDMI_OUTPUT.SBITS"%(options.gtx)))
 
-        print "Lock status:  %10s  %13s"%("QPLL","QPLL FPGA PLL")
-        print "                     0x%x            0x%x"%(
-                clocking["qplllock"],
-                clocking["qpllfpgaplllock"])
-        errorCounts = {}
-        errorCounts["QPLL"] = []
-        errorCounts["FPGA"] = []
-        for trial in range(options.errorRate):
-                errorCounts["QPLL"].append(calculateLockErrors(optohybrid,options.gtx,"QPLL",SAMPLE_TIME))
-                errorCounts["FPGA"].append(calculateLockErrors(optohybrid,options.gtx,"QPLL_FPGA_PLL",SAMPLE_TIME))
-        
-        qrates = getErrorRate(errorCounts["QPLL"],SAMPLE_TIME)
-        frates = getErrorRate(errorCounts["FPGA"],SAMPLE_TIME)
-        print "Unlock count: 0x%08x     0x%08x"%(qrates[0],frates[0])
-        0x00000107
-        print "Unlock rate:%10sHz   %10sHz"%("%2.2f%s"%(qrates[1],qrates[2]),"%2.2f%s"%(frates[1],frates[2]))
+    print "Lock status:  %10s  %13s"%("QPLL","QPLL FPGA PLL")
+    print "                     0x%x            0x%x"%(
+            clocking["qplllock"],
+            clocking["qpllfpgaplllock"])
+    errorCounts = {}
+    errorCounts["QPLL"] = []
+    errorCounts["FPGA"] = []
+    for trial in range(options.errorRate):
+        errorCounts["QPLL"].append(calculateLockErrors(optohybrid,options.gtx,"QPLL",SAMPLE_TIME))
+        errorCounts["FPGA"].append(calculateLockErrors(optohybrid,options.gtx,"QPLL_FPGA_PLL",SAMPLE_TIME))
+        pass
+    qrates = getErrorRate(errorCounts["QPLL"],SAMPLE_TIME)
+    frates = getErrorRate(errorCounts["FPGA"],SAMPLE_TIME)
+    print "Unlock count: 0x%08x     0x%08x"%(qrates[0],frates[0])
+    0x00000107
+    print "Unlock rate:%10sHz   %10sHz"%("%2.2f%s"%(qrates[1],qrates[2]),"%2.2f%s"%(frates[1],frates[2]))
 else:
-        print "Sources:  %6s  %7s  %9s"%("TrgSrc","SBitSrc","RefCLKSrc")
-        print "             0x%x      0x%x        0x%x"%(
-                readRegister(optohybrid,"GLIB.OptoHybrid_%d.OptoHybrid.CONTROL.TRIGGER.SOURCE"%(options.gtx)),
-                readRegister(optohybrid,"GLIB.OptoHybrid_%d.OptoHybrid.CONTROL.HDMI_OUTPUT.SBITS"%(options.gtx)),
-                clocking["refclock"])
+    print "Sources:  %6s  %7s  %9s"%("TrgSrc","SBitSrc","RefCLKSrc")
+    print "             0x%x      0x%x        0x%x"%(
+            readRegister(optohybrid,"GEM_AMC.OH.OH%d.CONTROL.TRIGGER.SOURCE"%(options.gtx)),
+            readRegister(optohybrid,"GEM_AMC.OH.OH%d.CONTROL.OUTPUT.SBITS"%(options.gtx)),
+            clocking["refclock"])
 
-        print "Lock status:  %8s  %7s  %4s  %3s"%("FPGA PLL","EXT PLL","CDCE","GTX")
-        print "                   0x%x      0x%x   0x%x  0x%x"%(
-                clocking["fpgaplllock"],
-                clocking["extplllock"],
-                clocking["cdcelock"],
-                clocking["gtxreclock"])
-	
-print 
+    print "Lock status:  %8s  %7s  %4s  %3s"%("FPGA PLL","EXT PLL","CDCE","GTX")
+    print "                   0x%x      0x%x   0x%x  0x%x"%(
+            clocking["fpgaplllock"],
+            clocking["extplllock"],
+            clocking["cdcelock"],
+            clocking["gtxreclock"])
+    pass
+print
 #print "-> OH Clocking (src, bkp):     VFAT         CDCE"
 #print "-> %22s%d       (0x%x  0x%x)   (0x%x  0x%x)"%("link",links[link],
 #						     clocking["vfatsrc"],clocking["vfatbkp"],
@@ -149,7 +149,7 @@ for key in counters["T1"]:
                                                            counters["T1"][key]["CalPulse"],
                                                            counters["T1"][key]["Resync"],
                                                            counters["T1"][key]["BC0"])
-	
+
 print
 print "--=======================================--"
 print "-> OH: %10s  %12s"%("ErrCnt","(rate)")
@@ -164,11 +164,11 @@ print "-> TRK: 0x%08x  (%6.2f%1sHz)"%(rates["TRK"][0],rates["TRK"][1],rates["TRK
 print "-> TRG: 0x%08x  (%6.2f%1sHz)"%(rates["TRG"][0],rates["TRG"][1],rates["TRG"][2])
 
 print
-print "FIFO:  %8s  %7s  %10s"%("isEmpty",  "isFull", "depth")
-fifoInfo = readFIFODepth(glib,options.gtx)
-print "       %8s  %7s  %10s"%("0x%x"%(fifoInfo["isEMPTY"]),
-                               "0x%x"%(fifoInfo["isFULL"]),
-                               "0x%x"%(fifoInfo["Occupancy"]))
-
-print
+# print "FIFO:  %8s  %7s  %10s"%("isEmpty",  "isFull", "depth")
+# fifoInfo = readFIFODepth(glib,options.gtx)
+# print "       %8s  %7s  %10s"%("0x%x"%(fifoInfo["isEMPTY"]),
+#                                "0x%x"%(fifoInfo["isFULL"]),
+#                                "0x%x"%(fifoInfo["Occupancy"]))
+#
+#print
 print "--=======================================--"
