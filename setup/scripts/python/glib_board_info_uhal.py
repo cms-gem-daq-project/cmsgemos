476b791b81ec14711787a2a41aa3c33b9b4eb3bd
Jared Sturdy <sturdy@cern.ch>
2016-09-19 18:36:41 +0200

    update to the glib python script for VTT/Rx switching

--- setup/scripts/python/glib_board_info_uhal.py
+++ setup/scripts/python/glib_board_info_uhal.py
@@ -87,10 +87,10 @@ print "  Opening GLIB with IP", ipaddr
 print "--=======================================--"
 print
 
-#if options.vtrx :
-#        writeRegister(glib,"GLIB.LINK_CONTROL.TX_Polarity.SFP0",0x0)
-#else:
-#        writeRegister(glib,"GLIB.LINK_CONTROL.TX_Polarity.SFP0",0x1)
+if options.vtrx :
+        writeRegister(glib,"GEM_AMC.GEM_SYSTEM.TK_LINK_TX_POLARITY",0x0)
+else:
+        writeRegister(glib,"GEM_AMC.GEM_SYSTEM.TK_LINK_TX_POLARITY",0x1)
 
 if not options.userOnly:
 	getSystemInfo(glib)
