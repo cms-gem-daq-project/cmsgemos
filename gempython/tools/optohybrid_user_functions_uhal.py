df5e395a494acf51a1b861054a8f8bdf99e64bd1
Jared Sturdy <jsturdy@users.noreply.github.com>
2017-03-14 21:59:56 +0100

    Update optohybrid_user_functions_uhal.py
    * Fixed bug in sysmon reading
    * Added placeholder for VFAT ADC functions

--- gempython/tools/optohybrid_user_functions_uhal.py
+++ gempython/tools/optohybrid_user_functions_uhal.py
@@ -709,6 +709,12 @@ def getADCVCCInt(device, gtx, debug=False):
 def getADCVCCAux(device, gtx, debug=False):
     return getADCValue(device,gtx,"VCCAUX",debug)
 
+def getVFATADCVoltage(device, gtx, column, debug=False):
+    return getADCValue(device,gtx,"VCCINT",debug)
+
+def getVFATADCCurrent(device, gtx, column, debug=False):
+    return getADCValue(device,gtx,"VCCAUX",debug)
+
 def printSysmonInfo(device, gtx, debug=False):
     adcBase = "GEM_AMC.OH.OH%d.ADC"%(gtx)
 
@@ -719,7 +725,7 @@ def printSysmonInfo(device, gtx, debug=False):
     for reg in regList:
         regs.append("%s.%s"%(adcBase,reg))
         pass
-    readRegisterList(device,regs)
+    res = readRegisterList(device,regs)
     print "OptoHybrid ADC sysmon"
     print "        %8s  %8s  %8s"%("Temp", "VCCINT", "VCCAUX")
     print "Current:%2.6f  %2.6f  %2.6f"%(res["%s.TEMP"%(adcBase)],
