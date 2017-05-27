039afcffdd6dbf6d3dc546a13f14412c263916ba
Jared Sturdy <sturdy@cern.ch>
2017-05-27 16:42:48 +0200

    updated some travisCI parts

---
+++ .travis/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -xe
+
+# Thanks to:
+# https://github.com/opensciencegrid/htcondor-ce/tree/master/tests
+
+OS_VERSION=$1
+
+ls -l /home
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+uname -a
+
+make -j4
+
