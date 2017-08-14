83f0023ab410fb53e64b07edcc903eeb83c93e7c
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-08-14 11:12:08 +0200

    Final updates to gempython/utils/db

--- gempython/utils/db/settings.py
+++ gempython/utils/db/settings.py
@@ -15,3 +15,20 @@ DATABASES = {
         'PASSWORD': 'gemdaq',
     }
 }
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gempython.utils.db.settings.local")
+SECRET_KEY = 'ef7$_)r0$#(x)ovd^m_6_bevsonkaxalvrhds9@ilb_p0o&-xn'
+
+INSTALLED_APPS = [
+    'gempython.utils.db.ldqm_db',
+    ]
+DATABASES = {
+    'default': {
+        'ENGINE':  'django.db.backends.mysql',
+        'NAME':     'ldqm_tif_qc8_db',
+        'PORT':     3306,
+        'HOST':     'cosmicstandtif',
+        'USER':     'gemdaq',
+        'PASSWORD': 'gemdaq',
+    }
+}
