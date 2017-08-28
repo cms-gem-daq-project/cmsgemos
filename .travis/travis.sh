78afbde2f97a0372989de58e55f0f9e4c1e74c0d
Jared Sturdy <sturdy@cern.ch>
2017-08-28 18:11:15 +0200

    Updated the travis config for the standard CERN docker images

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -7,13 +7,16 @@
 # Version of CentOS/RHEL
 el_version=$1
 
-docker_image=gitlab-registry.cern.ch/sturdy/gemdaq_ci_worker:slc6
 # Run tests in Container
-if [ "$el_version" = "6" ]
+if [ "$el_version" == "6" ]
 then
+    # docker_image=gitlab-registry.cern.ch/sturdy/gemdaq_ci_worker:slc6
+    docker_image=cern/slc6-base
     sudo docker run ${docker_image} /bin/bash -c "bash -xe ./.travis/docker.sh ${OS_VERSION}"
-elif [ "$el_version" = "7" ]
+elif [ "$el_version" == "7" ]
 then
+    # docker_image=gitlab-registry.cern.ch/sturdy/gemdaq_ci_worker:slc6
+    docker_image=cern/cc7-base
     docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup $docker_image /usr/sbin/init
     DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
     docker logs $DOCKER_CONTAINER_ID
