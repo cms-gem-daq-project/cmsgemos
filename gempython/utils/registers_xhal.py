ef26473d1665765cf865e5554b0e9b19c8a14a6b
Brian Dorney <brian.l.dorney@cern.ch>
2017-10-16 09:54:20 +0200

    rpc_conncet will now throw exception if connection is not opened successfully

--- gempython/utils/registers_xhal.py
+++ gempython/utils/registers_xhal.py
@@ -16,7 +16,7 @@ class rpcService:
 
         # Define broadcast read
         self.broadcastRead = self.lib.broadcastRead
-        self.broadcastRead.argtypes = [c_uint, c_char_p, c_uint]
+        self.broadcastRead.argtypes = [c_uint, c_char_p, c_uint, POINTER(c_uint32)]
         self.broadcastRead.restype = c_uint
 
         # Define broadcast write
@@ -24,7 +24,27 @@ class rpcService:
         self.broadcastWrite.argtypes = [c_uint, c_char_p, c_uint, c_uint]
         self.broadcastWrite.restype = c_uint
 
+        # Define VFAT3 Configuration
+        #configureVFAT3s = lib.configureVFAT3s
+        #configureVFAT3s.argTypes = [ c_uint, c_uint ]
+        #configureVFAT3s.restype = c_uint
+        
+        # Define TTC Configuration
+        ttcGenConf = self.lib.ttcGenConf
+        ttcGenConf.restype = c_uint
+        ttcGenConf.argtypes = [c_uint, c_uint]
+
+        # Define v3 scan module
+        #genScan = lib.genScan
+        #genScan.restype = c_uint
+        #genScan.argtypes = [c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_char_p, POINTER(c_uint32)]
+
         return
 
     def connect(self, amc="eagle26"):
-        self.rpc_connect(amc)
+        try:
+            if 0 != self.rpc_connect(amc):
+                print("Failed to open RPC connection for device %s"%(amc))
+                sys.exit(os.EX_SOFTWARE)
+        except Exception as e:
+            print e
