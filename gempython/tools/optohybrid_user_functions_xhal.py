fe893d14861adffb23dfe697f8f0af7c015954bc
Brian Dorney <brian.l.dorney@cern.ch>
2017-11-02 19:03:27 +0100

    updated OH xhal functionality

--- gempython/tools/optohybrid_user_functions_xhal.py
+++ gempython/tools/optohybrid_user_functions_xhal.py
@@ -1,24 +1,19 @@
 import os, signal, sys, time
 sys.path.append('${GEM_PYTHON_PATH}')
 
-#from gempython.tools.amc_user_functions_uhal import getAMCObject
 from gempython.tools.amc_user_functions_xhal import *
-#from gempython.tools.glib_system_info_uhal import getSystemFWVer
 from gempython.utils.gemlogger import colormsg
 from gempython.utils.nesteddict import nesteddict
-#from gempython.utils.registers_uhal import *
-#from gempython.utils.registers_xhal import *
 
 import logging
-#ohlogger = logging.getLogger(__name__)
 
-class scanmode:
-    THRESHTRG = 0 # Threshold scan
-    THRESHCH  = 1 # Threshold scan per channel
-    LATENCY   = 2 # Latency scan
-    SCURVE    = 3 # s-curve scan
-    THRESHTRK = 4 # Threshold scan with tracking data
-    pass
+#class scanmode:
+#    THRESHTRG = 0 # Threshold scan
+#    THRESHCH  = 1 # Threshold scan per channel
+#    LATENCY   = 2 # Latency scan
+#    SCURVE    = 3 # s-curve scan
+#    THRESHTRK = 4 # Threshold scan with tracking data
+#    pass
 
 class HwOptoHybrid:
     def __init__(self, slot, link, shelf=1, debug=False):
@@ -46,20 +41,23 @@ class HwOptoHybrid:
         self.bWrite.argtypes = [c_uint, c_char_p, c_uint, c_uint]
         self.bWrite.restype = c_uint
        
-        # Define the v3 scan module
+        # Define the scan module
         self.genScan = self.parentAMC.lib.genScan
         self.genScan.restype = c_uint
-        self.genScan.argtypes = [c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_char_p, POINTER(c_uint32)]
+        self.genScan.argtypes = [c_uint, c_uint, c_uint, c_uint, 
+                                 c_uint, c_uint, c_uint, c_uint, 
+                                 c_char_p, c_bool, POINTER(c_uint32)]
+
+        self.KnownV2bElScanRegs = [
+                    "Latency",
+                    "VCal",
+                    "VThreshold1",
+                    "VThreshold1PerChan",
+                    "VThreshold1Trk"
+                ]
 
         return
 
-    #def checkOHBoard(self):
-    #    # TO BE IMPLEMENTED
-    #    return True
-        
-    #def getBoard(self):
-    #    return self.ohboard
-    
     def broadcastRead(self,register,mask=0xff000000):
         """
         Perform a broadcast RPC read on the VFATs specified by mask
@@ -95,11 +93,47 @@ class HwOptoHybrid:
 
         return rpcResp
 
+    def performCalibrationScan(ohN, chan, scanReg, outData, enableCal=1, nevts=1000, dacMin=0, dacMax=254, stepSize=1, mask=0x0, useUltra=True):
+        """
+        Performs either a v2b ultra scan or a v3 generic scan
+
+        ohN         - Optohybrid number (e.g. link)
+        chan        - VFAT channel to be scanned
+        scanReg     - Name of register to be scanned.  For v3
+                      electronics consult the address table.
+                      For v2b electronics see self.KnownV2bElScanRegs
+        outData     - Array of type c_uint32, array size must be:
+                      ((dacMax - dacMin + 1) / stepSize) * 24.
+                      The first ((dacMax - dacMin + 1) / stepSize)
+                      array positions are for VFAT0, the next
+                      ((dacMax - dacMin + 1) / stepSize) are for VFAT1,
+                      etc...  If a VFAT is masked entries in the array
+                      are still allocated but assigned a 0 value.
+        enableCal   - V3 electronics only. Enable cal pulse
+        nevts       - Number of events for each dac value in scan
+        dacMin      - Starting dac value of the scan
+        dacMax      - Ending dac value of the scan
+        stepSize    - Step size for moving from dacMin to dacMax
+        mask        - VFAT mask to use
+        useUltra    - V2b electronics only, perform an ultra scan 
+                      instead of a FW scan.  Note if false the VFAT
+                      to be scanned is taken as the first non-masked
+                      VFAT defined in mask
+        """
+
+        if self.parentAMC.fwVersion < 3:
+            if scanReg not in self.KnownV2bElScanRegs:
+                print("Parent AMC Major FW Version: %i"%(self.parentAMC.fwVersion))
+                print("V2b Electronics Behavior Detected")
+                print("scanReg %s Not Known")
+                print("Available scanReg values are:", self.KnownV2bElScanRegs)
+                sys.exit(os.EX_USAGE)
+        else:
+            if "CFG_" in scanReg:
+                scanReg = str.replace("CFG_","")
+
+        return self.genScan(nevts, ohN, dacMin, dacMax, stepSize, chan, enableCal, mask, scanReg, useUltra, outData)
+
     def setDebug(self, debug):
         self.debug = debug
         return
-
-#def getOHObject(slot,link,shelf=1,debug=False):
-#    hwOH = HwOptoHybrid(slot, link, shelf, debug)
-#    #return hwOH.getBoard()
-#    return hwOH
