f215226d6ebcff87f989b03781b4f757b4de7c45
Jared Sturdy <sturdy@cern.ch>
2018-04-19 09:18:31 +0200

    structure for python packaging

---
+++ config/mfPythonRPM.mk
@@ -0,0 +1,158 @@
+# Created with insights from
+## amc13/config/mfPythonRPMRules.mk
+
+INSTALL_PATH = /opt/cmsgemos
+ProjectPath  ?= $(BUILD_HOME)/$(Project)
+PackagePath  ?= $(BUILD_HOME)/$(Project)/$(Package)
+RPMBUILD_DIR = $(PackagePath)/rpm
+# 
+CMSGEMOS_OS ?= $(XDAQ_OS)
+
+ifndef PythonModules
+	$(error Python module names missing "PythonModules")
+endif
+
+.PHONY: _all
+.PHONY: rpm _rpmall _rpmprep _setup_update _rpmbuild _rpmdevbuild _rpmsetup
+rpm: _rpmbuild
+	@echo "Running rpm target"
+#	cat $(RPMBUILD_DIR)/setup.py
+
+#_rpmall: _all _rpmprep _setup_update _rpmsetup _rpmbuild
+_rpm: _all _rpmbuild
+	@echo "Running _rpmall target"
+#	cat $(RPMBUILD_DIR)/setup.py
+# Copy the package skeleton
+# Ensure the existence of the module directory
+# Copy the libraries into python module
+_rpmsetup: _rpmprep _setup_update
+# Change directory into pkg and copy everything into rpm build dir
+	@echo "Running _rpmsetup target"
+#	cat $(RPMBUILD_DIR)/setup.py
+	cd pkg && \
+	find . -iname 'setup.*' -prune -o -name "*" -exec install -D \{\} $(RPMBUILD_DIR)/\{\} \;
+# Add a manifest file (may not be necessary
+#	echo "include */*.so" > $(RPMBUILD_DIR)/MANIFEST.in
+
+_rpmbuild: _rpmsetup
+	@echo "Running _rpmbuild target"
+#	cat $(RPMBUILD_DIR)/setup.py
+	cd $(RPMBUILD_DIR) && python setup.py bdist_rpm \
+	--release python$(PYTHON_VERSION) \
+#	--release $(CMSGEMOS_OS).python$(PYTHON_VERSION)
+	--binary-only --force-arch=`uname -m`
+# Harvest the crop
+	find rpm -name "*.rpm"    -exec cp -a \{\} rpm/ \;
+	find rpm -name "*.tar.gz" -exec cp -a \{\} rpm/ \;
+	find rpm -name "*.tgz"    -exec cp -a \{\} rpm/ \;
+	find rpm -name "*.tbz2"   -exec cp -a \{\} rpm/ \;
+
+_setup_update:
+	@echo "Running _setup_update target"
+	$(MakeDir) $(PackagePath)/rpm/RPMBUILD
+
+	if [ -e $(PackagePath)/pkg/setup.py ]; then \
+		echo Found $(PackagePath)/pkg/setup.py; \
+		cp $(PackagePath)/pkg/setup.py $(RPMBUILD_DIR)/setup.py; \
+	elif [ -e $(PackagePath)/setup.py ]; then \
+		echo $(PackagePath) found setup.py; \
+		cp $(PackagePath)/setup.py $(RPMBUILD_DIR)/setup.py; \
+	elif [ -e $(PackagePath)/setup/setup.py ]; then \
+		echo Found $(PackagePath)/setup/setup.py; \
+		cp $(PackagePath)/setup/setup.py $(RPMBUILD_DIR)/setup.py; \
+	elif [ -e $(PackagePath)/setup/build/setup.py ]; then \
+		echo Found $(PackagePath)/setup/build/setup.py; \
+		cp $(PackagePath)/setup/build/setup.py $(RPMBUILD_DIR)/setup.py; \
+	elif [ -e $(ProjectPath)/setup/config/setupTemplate.py ]; then \
+		echo Found $(ProjectPath)/setup/config/setupTemplate.py; \
+		cp $(ProjectPath)/setup/config/setupTemplate.py $(RPMBUILD_DIR)/setup.py; \
+	elif [ -e $(BUILD_HOME)/config/build/setupTemplate.py ]; then \
+		echo Found $(BUILD_HOME)/config/build/setupTemplate.pyz; \
+		cp $(BUILD_HOME)/config/build/setupTemplate.py $(RPMBUILD_DIR)/setup.py; \
+	else \
+		echo Unable to find any setupTemplate.py; \
+		exit 1; \
+	fi
+
+	sed -i 's#__author__#$(Packager)#'                $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__project__#$(Project)#'                $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__summary__#None#'                      $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__gitrev__#$(GITREV)#'                  $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__builddate__#$(BUILD_DATE)#'           $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__package__#$(Package)#'                $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__packagedir__#$(PackagePath)#'         $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__packagename__#$(PackageName)#'        $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__longpackage__#$(LongPackage)#'        $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__pythonmodules__#$(PythonModules)#'    $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__version__#$(PACKAGE_FULL_VERSION)#'   $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__release__#$(CMSGEMOS_OS)#'            $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__prefix__#$(GEMPYTHON_ROOT)#'          $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__os__#$(CMSGEMOS_OS)#'                 $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__platform__#$(CMSGEMOS_PLATFORM)#'     $(RPMBUILD_DIR)/setup.py
+	sed -i 's#__description__#None#'                  $(RPMBUILD_DIR)/setup.py
+#	cat $(RPMBUILD_DIR)/setup.py
+
+	if [ -e $(PackagePath)/pkg/setup.cfg ]; then \
+		echo Found $(PackagePath)/pkg/setup.cfg; \
+		cp $(PackagePath)/pkg/setup.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	elif [ -e $(PackagePath)/setup.cfg ]; then \
+		echo Found $(PackagePath)/setup.cfg; \
+		cp $(PackagePath)/setup.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	elif [ -e $(PackagePath)/setup/setup.cfg ]; then \
+		echo Found $(PackagePath)/setup/setup.cfg; \
+		cp $(PackagePath)/setup/setup.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	elif [ -e $(PackagePath)/setup/build/setup.cfg ]; then \
+		echo Found $(PackagePath)/setup/build/setup.cfg; \
+		cp $(PackagePath)/setup/build/setup.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	elif [ -e $(ProjectPath)/setup/config/setupTemplate.cfg ]; then \
+		echo Found $(ProjectPath)/setup/config/setupTemplate.cfg; \
+		cp $(ProjectPath)/setup/config/setupTemplate.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	elif [ -e $(BUILD_HOME)/config/build/setupTemplate.cfg ]; then \
+		echo Found $(BUILD_HOME)/config/setupTemplate.cfg; \
+		cp $(BUILD_HOME)/config/build/setupTemplate.cfg $(RPMBUILD_DIR)/setup.cfg; \
+	else \
+		echo Unable to find any setupTemplate.cfg; \
+		exit 1; \
+	fi
+
+	sed -i 's#__author__#$(Packager)#'                $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__project__#$(Project)#'                $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__summary__#None#'                      $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__gitrev__#$(GITREV)#'                  $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__builddate__#$(BUILD_DATE)#'           $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__package__#$(Package)#'                $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__packagedir__#$(PackagePath)#'         $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__packagename__#$(PackageName)#'        $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__longpackage__#$(LongPackage)#'        $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__pythonmodules__#$(PythonModules)#'    $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__version__#$(PACKAGE_FULL_VERSION)#'   $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__release__#$(CMSGEMOS_OS)#'            $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__prefix__#$(GEMPYTHON_ROOT)#'          $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__os__#$(CMSGEMOS_OS)#'                 $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__platform__#$(CMSGEMOS_PLATFORM)#'     $(RPMBUILD_DIR)/setup.cfg
+	sed -i 's#__description__#None#'                  $(RPMBUILD_DIR)/setup.cfg
+
+
+.PHONY: cleanrpm _cleanrpm
+cleanrpm: _cleanrpm
+	@echo "Running cleanrpm target"
+
+_cleanrpm:
+	@echo "Running _cleanrpm target"
+	-rm -rf $(PackagePath)/rpm
+
+.PHONY: _cleanrpmall
+_cleanrpmall:
+	-rm -rf $(PackagePath)/rpm
+
+
+.PHONY: _installrpmall
+_installrpmall:
+	@mkdir -p $(INSTALL_PREFIX)/rpm
+	-cp $(PackagePath)/rpm/*.rpm $(INSTALL_PREFIX)/rpm
+
+
+.PHONY: _changelogall
+_changelogall:
+	@cd $(PackagePath); \
+	git log --full-diff --decorate=full < ChangeLog
---
+++ gempython/pkg/setup.py
@@ -0,0 +1,60 @@
+
+from setuptools import setup,find_packages
+
+from os import listdir
+from os.path import isfile,join
+scriptdir  = 'gempython/bin'
+scriptpath = '/opt/cmsgemos/bin'
+scripts    = listdir(scriptdir)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def getscripts():
+    # would prefer to use this for executables, if one can control the install location
+    # to be user defined rather than /usr/bin
+    # return dict(('{0:s}/{1:s}'.format(scriptpath,x),
+    #       '{0:s}/{1:s}'.format(scriptdir,x)) for x in scripts if isfile(join(scriptdir,x)) )
+    return ['{0:s}/{1:s}'.format(scriptdir,x) for x in scripts if isfile(join(scriptdir,x)) ]
+
+def getpkgdata():
+    # actual package data
+    data = dict((pkg,['*.txt','*.so']) for pkg in __pythonmodules__)
+    # hack just to get the build to work
+    data['gempython/bin'] = ['gempython/bin/*.py']
+    return data
+
+def getreqs():
+    # with open('gempython/requirements.txt') as f:
+    #     reqs = f.readlines()
+    #     return [x.strip() for x in reqs]
+    pass
+
+setup(name             = '__packagename__',
+      version          = '__version__',
+      # use_scm_version  = True,
+      description      = '__description__',
+      long_description = readme(),
+      # author           = __author__,
+      author           = 'GEM Online Systems Group',
+      # author_email     = __author_email__,
+      author_email     = 'cms-gem-online-sw@cern.ch',
+      # url              = __url__,
+      url              = 'https://cms-gem-daq-project.github.io/gem-plotting-tools',
+      install_requires = getreqs(),
+      # install_requires = ['numpy>=1.7', 'root_numpy>=4.7'],
+      # scripts          = getscripts(),
+      # build_requires   = '__build_requires__',
+      # namespace_package = "gempython",
+      # packages         = __pythonmodules__, # for PEP420 native namespace util
+      packages           = find_packages(), # for pkgutil namespace method
+      # package_dir      = {'' : ''},
+      package_data     = getpkgdata(),
+      # dependency_links   = ['http://cmsgemos.web.cern.ch/cmsgemos/repo/tarball/master#egg=package-1.0']
+      zip_safe         = False,
+      data_files       = [('/opt/cmsgemos/bin', ['{0:s}/{1:s}'.format(scriptdir,x) for x in scripts if isfile(join(scriptdir,x))])],
+      build_requires   = ['importlib; python_version=="2.6"','setuptools>=25.0','setuptools_scm'],
+      setup_requires   = ['importlib; python_version=="2.6"','setuptools>=25.0'],
+      license          = '__license__',
+)
