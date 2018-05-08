924c0b46f457eb49c6c840bce6111d9b27117a89
Jared Sturdy <sturdy@cern.ch>
2018-05-08 14:22:28 +0200

    update XasS related scripts

--- gemxaas/buildXaaSProfiles.sh
+++ gemxaas/buildXaaSProfiles.sh
@@ -7,18 +7,25 @@ then
 elif [ "$XDAQ_PLATFORM" == "x86_64_centos7" ]
 then
     xdaqver="14"
+else
+    xdaqver="15"
 fi
 
-BUILD_DIR=/tmp/${USER}/daq/xaas
-echo svn co -N svn+ssh://sturdy@svn.cern.ch/reps/cmsos/releases/baseline${xdaqver}/trunk/daq/xaas ${BUILD_DIR}
-# echo cd ${BUILD_DIR}
-echo svn up ${BUILD_DIR}/template
-echo svn up -N ${BUILD_DIR}/slim
-# echo cd slim
-echo svn up ${BUILD_DIR}/slim/template
-echo cp -rfp ${BUILD_HOME}/${GEM_OS_PROJECT}/gemxaas/gem* ${BUILD_DIR}/slim/
-echo cp -rfp ${BUILD_HOME}/${GEM_OS_PROJECT}/gemxaas/Makefile ${BUILD_DIR}/slim
+XAAS_DIR=/tmp/${USER}/daq/xaas
+echo svn co -N svn+ssh://sturdy@svn.cern.ch/reps/cmsos/releases/baseline${xdaqver}/trunk/daq/xaas ${XAAS_DIR}
 
-echo cd ${BUILD_DIR}/slim
+echo svn up ${XAAS_DIR}/template
+echo svn up -N ${XAAS_DIR}/slim
+
+if [[ ${xdaqver} =~ 13 ]]
+then
+    echo svn up ${XAAS_DIR}/slim/template
+fi
+
+echo cp -rfp ${BUILD_HOME}/${GEM_OS_PROJECT}/gemxaas/gem* ${XAAS_DIR}/slim/
+echo cp -rfp ${BUILD_HOME}/${GEM_OS_PROJECT}/gemxaas/Makefile ${XAAS_DIR}/slim
+
+echo cd ${XAAS_DIR}/slim
+
 echo make
 echo make rpm
---
+++ gemxaas/updateZones.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#############################################
+# Modify common makefiles in gembase package
+# Copy scritps from setup/etc
+# Need specific connection file maybe
+#############################################
+
+# zone=$1
+# modifier=$2
+# if [ -z ${2+x} ]
+# then
+#     echo modifier unset
+# else
+#     echo Set modifier to  $modifier
+#fi
+
+zones=(
+    gem904
+    gem904int
+    gem904qc8
+    gemp5
+    gemp5dev
+    gemp5pro
+)
+modifiers=(
+    "for generic GEM setup"
+    "for 904 .cms904 setup"
+    "for 904 integration setup"
+    "for 904 QC8 setup"
+    "for GEM P5 .cms setup"
+    "for GEM P5 development setup"
+    "for GEM P5 production setup"
+)
+dirs=(
+    addon
+    client
+    config
+    extension
+    service
+    settings
+    zone
+)
+
+idx=0
+for zone in "${zones[@]}"
+do
+    for dir in "${dirs[@]}"
+    do
+        cp gembase/$dir/Makefile $zone/$dir/
+    done
+    modifier="${modifiers[$idx]}"
+    perl -pi -e "s|for generic GEM setup|${modifier}|g" ./*/Makefile
+    cp ../setup/etc/addresstables/linkuhaltables.sh $zone/config/scripts/
+    cp ../setup/etc/profile.d/gemdaqenv.sh $zone/config/scripts/
+    let "idx++"
+done
