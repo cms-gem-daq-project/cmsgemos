3dd4d863c763050f7a21e036156c180229379af5
Jared Sturdy <sturdy@cern.ch>
2018-05-14 10:24:04 +0200

    fix file permissions in RPM

--- gempython/pkg/installrpm.sh
+++ gempython/pkg/installrpm.sh
@@ -6,10 +6,16 @@ python setup.py install --single-version-externally-managed -O1 --root=$RPM_BUIL
 # install 'scripts' to /opt/cmsgemos/bin
 mkdir -p %{buildroot}/opt/cmsgemos/bin
 cp -rfp gempython/scripts/*.py %{buildroot}/opt/cmsgemos/bin/
+find %{buildroot} -type f -exec chmod a+r {} \;
+find %{buildroot} -type f -iname '*.cfg' -exec chmod a-x {} \;
 
 # set permissions
 cat <<EOF >>INSTALLED_FILES
 %attr(-,root,root) /opt/cmsgemos/bin/*.py
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.py
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyo
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyc
+%attr(0755,root,root) /usr/lib/python*/site-packages/gempython/scripts/*.py
 EOF
 echo "Modified INSTALLED_FILES"
 cat INSTALLED_FILES
