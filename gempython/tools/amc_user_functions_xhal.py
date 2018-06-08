58a224e4eceba061012e99c31392548e4e9d03fd
Brian Dorney <brian.l.dorney@cern.ch>
2018-06-08 17:07:05 +0200

    adding v3 xhal functionality

---
+++ gempython/tools/amc_user_functions_xhal.py
@@ -0,0 +1,343 @@
+import os, signal, sys
+sys.path.append('${GEM_PYTHON_PATH}')
+
+from ctypes import *
+from gempython.utils.gemlogger import colormsg
+from gempython.utils.nesteddict import nesteddict
+from gempython.utils.wrappers import envCheck
+from time import sleep
+
+from rw_reg import Node, parseInt, parseXML
+
+import logging
+
+gMAX_RETRIES = 5
+gRetries = 5
+
+class colors:
+        WHITE   = '\033[97m'
+        CYAN    = '\033[96m'
+        MAGENTA = '\033[95m'
+        BLUE    = '\033[94m'
+        YELLOW  = '\033[93m'
+        GREEN   = '\033[92m'
+        RED     = '\033[91m'
+        ENDC    = '\033[0m'
+
+class ctp7Params:
+    # Key (shelf,slot); val = eagleXX
+    # See: https://twiki.cern.ch/twiki/bin/view/CMS/GEMDAQExpert#List_of_CTP7_s
+    cardLocation = { 
+            (3,2):"eagle26", #QC8
+            (2,5):"eagle60",
+            #(?,?):"eagle61",
+            (1,3):"eagle33", #P5
+            (1,2):"eagle34"} #Coffin
+            #(1,?):"eagle64"}
+
+class HwAMC:
+    def __init__(self, slot, shelf=1, debug=False):
+        """
+        Initialize the HW board an open an RPC connection
+        """
+        envCheck("XHAL_ROOT")
+        #self.addrTable = os.getenv("XHAL_ROOT")+'/etc/gem_amc_top.xml'
+        
+        # Debug flag
+        self.debug = debug
+        
+        # Logger
+        self.amclogger = logging.getLogger(__name__)
+
+        # Store HW info
+        self.name = ctp7Params.cardLocation[(shelf,slot)]
+        self.shelf = shelf
+        self.slot = slot
+
+        # Define the connection
+        self.lib = CDLL(os.getenv("XHAL_ROOT")+"/lib/x86_64/librpcman.so")
+        self.rpc_connect = self.lib.init
+        self.rpc_connect.argtypes = [c_char_p]
+        self.rpc_connect.restype = c_uint
+       
+        # Define read register
+        self.rReg = self.lib.getReg
+        self.rReg.restype = c_uint
+        self.rReg.argtypes=[c_uint]
+        
+        # Define read block
+        self.rBlock = self.lib.getBlock
+        self.rBlock.restype = c_uint
+        self.rBlock.argtypes=[c_uint,POINTER(c_uint32)]
+
+        # Define write register
+        self.wReg = self.lib.putReg
+        self.wReg.argtypes=[c_uint,c_uint]
+
+        # Define TTC Functions
+        self.ttcGenConf = self.lib.ttcGenConf
+        self.ttcGenConf.restype = c_uint
+        self.ttcGenConf.argtypes = [c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_bool]
+
+        self.ttcGenToggle = self.lib.ttcGenToggle
+        self.ttcGenToggle.restype = c_uint
+        self.ttcGenToggle.argtypes = [c_uint, c_bool]
+
+        # Parse XML
+        #self.nodes = parseXML(self.addrTable)
+        self.nodes = parseXML()
+
+        # Open RPC Connection
+        print "Initializing AMC", self.name
+        self.rpc_connect(self.name)
+        self.fwVersion = self.readRegister("GEM_AMC.GEM_SYSTEM.RELEASE.MAJOR") 
+        print "My FW release major = ", self.fwVersion
+
+        return
+
+    def blockL1A(self):
+        """
+        blocks L1A's from backplane for this AMC
+        """
+       
+        self.writeRegister("GEM_AMC.TTC.CTRL.L1A_ENABLE", 0x0)
+        return
+
+    def configureTTC(self, pulseDelay, L1Ainterval, ohN=0, mode=0, t1type=0,  nPulses=0, enable=True):
+        """
+        Default values reflects v3 electronics behavior
+
+        =======v3 electronics Behavior=======
+             pulseDelay (only for enable = true), delay between CalPulse and L1A
+             L1Ainterval (only for enable = true), how often to repeat signals
+             enable = true (false) ignore (take) ttc commands from backplane for this AMC (affects all links)
+        =======v2b electronics behavior=======
+             Configure the T1 controller
+             mode: 0 (Single T1 signal),
+                   1 (CalPulse followed by L1A),
+                   2 (pattern)
+             type (only for mode 0, type of T1 signal to send):
+                   0 L1A
+                   1 CalPulse
+                   2 Resync
+                   3 BC0
+             pulseDelay (only for mode 1), delay between CalPulse and L1A
+             L1Ainterval (only for mode 0,1), how often to repeat signals
+             nPulses how many signals to send (0 is continuous)
+             enable = true (false) start (stop) the T1Controller for link ohN
+        """
+
+        return self.ttcGenConf(ohN, mode, t1type, pulseDelay, L1Ainterval, nPulses, enable)
+
+    def enableL1A(self):
+        """
+        enables L1A's from backplane for this AMC
+        """
+        
+        self.writeRegister("GEM_AMC.TTC.CTRL.L1A_ENABLE", 0x1)
+        return
+
+    def getL1ACount(self):
+        return self.readRegister("GEM_AMC.TTC.CMD_COUNTERS.L1A")
+
+    def getNode(self,nodeName):
+        #return next((node for node in self.nodes if node.name == nodeName),None)
+        try: 
+            return self.nodes[nodeName]
+        except KeyError:
+            print "Node %s not found" %(nodeName)
+            return None
+
+    def getTTCStatus(self, ohN, display=False):
+        running = 0xdeaddead
+        if self.fwVersion < 3:
+            contBase = "GEM_AMC.OH.OH%i.T1Controller"%(ohN)
+            running = self.readRegister("%s.MONITOR"%(contBase))
+            if display:
+                print("Info for %s"%(contBase))
+                print("\tDELAY:\t\t%i"%(self.readRegister("%s.DELAY"%(contBase))))
+                print("\tINTERVAL:\t%i"%(self.readRegister("%s.INTERVAL"%(contBase))))
+                print("\tMODE:\t\t%i"%(self.readRegister("%s.MODE"%(contBase))))
+                print("\tMONITOR:\t%i"%(running))
+                print("\tNUMBER:\t\t%i"%(self.readRegister("%s.NUMBER"%(contBase))))
+                print("\tTYPE:\t\t%i"%(self.readRegister("%s.TYPE"%(contBase))))
+        else:
+            contBase = "GEM_AMC.TTC.GENERATOR"
+            running = self.readRegister("%s.ENABLE"%(contBase))
+            if display:
+                print("Info for %s"%(contBase))
+                print("\tCYCLIC_L1A_GAP: \t\t%i"%(self.readRegister("%s.CYCLIC_L1A_GAP"%(contBase))))
+                print("\tCYCLIC_CALPULSE_TO_L1A_GAP: \t%i"%(self.readRegister("%s.CYCLIC_CALPULSE_TO_L1A_GAP"%(contBase))))
+                print("\tENABLE: \t\t\t%i"%(running))
+        return running
+
+    def readAddress(self,address):
+        output = self.rReg(address) 
+        return '{0:#010x}'.format(parseInt(str(output)))
+
+    def readBlock(register, nwords, debug=False):
+        """
+        read block 'register'
+        returns 'nwords' values in the register
+        """
+        global gRetries
+        nRetries = 0
+        m_node = self.getNode(register)
+        if m_node is None:
+            print colors.MAGENTA,"NODE %s NOT FOUND" %(register),colors.ENDC
+            return 0x0
+    
+        #if debug:
+        #    print "Trying to read block of %d words\n" %(nwords)
+        #    print m_node.output()
+     
+        p = (c_uint32*nwords)()
+        words = []
+        #while (nRetries < gMAX_RETRIES):
+        if (debug):
+            print "reading %d words from register %s"%(nwords,register)
+            pass
+        #for i in range(nwords):
+        #   words.append(readRegister(device,register, False))
+        #if (debug):
+        #    print "reading result: %s" %(words)
+        #    pass
+        res = self.rBlock(m_node.real_address,p,len(p))
+        if (res == 0):
+            words = list(p)
+            if (debug):
+                print "ReadBlock result:\n"
+                print words
+                pass
+            return words
+        else:
+            print colors.RED, "error encountered, retried read operation (%d)"%(nRetries),colors.ENDC
+            nRetries += 1
+        return words
+
+    def readRegister(self, register, debug=False):
+        """
+        read register 'register' using remote procedure call
+        returns value of the register
+        """
+        global gRetries
+        nRetries = 0
+        m_node = self.getNode(register)
+        if m_node is None:
+            print colors.MAGENTA,"NODE %s NOT FOUND" %(register),colors.ENDC
+            return 0x0
+        elif 'r' not in m_node.permission:
+            print colors.MAGENTA,"No read permission for register %s" %(register),colors.ENDC
+            return 0x0
+        if debug:
+            print "Trying to read\n"
+            print m_node.output()
+            pass
+        while (nRetries<gRetries):
+            res = self.rReg(parseInt(m_node.real_address))
+            if res == 0xdeaddead:
+                print colors.MAGENTA,"Bus error encountered while reading (%s), retrying operation (%d,%d)"%(register,nRetries,gRetries),colors.ENDC
+                continue
+                #return 0xdeaddead
+            else:
+                if m_node.mask is not None:
+                    shift_amount=0
+                    for bit in reversed('{0:b}'.format(m_node.mask)):
+                        if bit=='0': shift_amount+=1
+                        else: break
+                    fin_res = (res&m_node.mask)>>shift_amount
+                else:
+                    fin_res = res
+                if debug: print "Read register result: %s" %(fin_res)
+                return fin_res
+        return 0xdeaddead
+
+    def toggleTTCGen(self, ohN, enable):
+        """
+        v3  electronics: enable = true (false) ignore (take) ttc commands from backplane for this AMC
+        v2b electronics: enable = true (false) start (stop) the T1Controller for link ohN
+        """
+
+        return self.ttcGenToggle(ohN, enable)
+
+    def writeReg(self, reg, value):
+        address = reg.real_address
+        if 'w' not in reg.permission:
+            return 'No write permission!'
+        
+        if self.debug:
+            print "Initial value to write: %s, register %s"% (value,reg.name)
+        
+        # Apply Mask if applicable
+        if reg.mask is not None:
+            shift_amount=0
+            for bit in reversed('{0:b}'.format(reg.mask)):
+                if bit=='0': shift_amount+=1
+                else: break
+            shifted_value = value << shift_amount
+            for i in range(10):
+                initial_value = self.readAddress(address)
+                try: initial_value = parseInt(initial_value) 
+                except ValueError: return 'Error reading initial value: '+str(initial_value)
+                if initial_value == 0xdeaddead:
+                    print "Writing masked reg %s : Error while reading, retry attempt (%s)"%(reg.name,i)
+                    sleep(0.1)
+                    continue
+                else: break
+            if initial_value == 0xdeaddead:
+                 print "Writing masked reg %s failed. Exiting..." %(reg.name)
+                 #sys.exit()
+            final_value = (shifted_value & reg.mask) | (initial_value & ~reg.mask)
+        else: final_value = value
+        output = self.wReg(parseInt(address),parseInt(final_value))
+        if output != final_value:
+            print "Writing masked reg %s failed. Exiting..." %(reg.name)
+            print "wReg output %s" % (output)
+            #sys.exit()
+        return str('{0:#010x}'.format(final_value)).rstrip('L')+'('+str(value)+')\twritten to '+reg.name
+
+    def writeRegister(self, register, value, debug=False):
+        """
+        write value 'value' into register 'register' using remote procedure call
+        """
+        global gRetries
+        nRetries = 0
+        m_node = self.getNode(register)
+        if m_node is None:
+            print colors.MAGENTA,"NODE %s NOT FOUND"%(register),colors.ENDC
+            return 0x0
+    
+        if debug:
+            print "Trying to write\n"
+            print m_node.output()
+            pass
+     
+        while (nRetries < gMAX_RETRIES):
+            rsp = self.writeReg(m_node, value)
+            if "permission" in rsp:
+                print colors.MAGENTA,"NO WRITE PERMISSION",colors.ENDC
+                return
+            elif "Error" in rsp:
+                print colors.MAGENTA,"write error encountered (%s), retrying operation (%d,%d)"%(register,nRetries,gRetries),colors.ENDC
+                return
+            elif "0xdeaddead" in rsp:
+                print colors.MAGENTA,"0xdeaddead found (%s), retrying operation (%d,%d)"%(register,nRetries,gRetries),colors.ENDC
+                return
+            else: 
+                return 
+            pass
+        pass
+
+    def writeRegisterList(self, regs_with_vals, debug=False):
+        """
+        write value 'value' into register 'register' using remote procedure call
+        from an input dict
+        """
+        global gRetries
+        nRetries = 0
+        while (nRetries < gMAX_RETRIES):
+            for reg in regs_with_vals.keys():
+                self.writeRegister(reg,regs_with_vals[reg],debug)
+                pass
+            return
+        pass
---
+++ gempython/tools/vfat_user_functions_xhal.py
@@ -0,0 +1,328 @@
+import os, signal, sys, time
+sys.path.append('${GEM_PYTHON_PATH}')
+
+from gempython.utils.nesteddict import nesteddict
+from gempython.tools.optohybrid_user_functions_xhal import *
+from gempython.utils.gemlogger import colors,colormsg
+
+import logging
+
+class HwVFAT:
+    def __init__(self, slot, link, shelf=1, debug=False):
+        """
+        Initialize the HW board an open an RPC connection
+        """
+        # Debug flag
+        self.debug = debug
+        
+        # Logger
+        self.vfatlogger = logging.getLogger(__name__)
+
+        # Optohybrid
+        self.parentOH = HwOptoHybrid(slot, link, shelf, debug)
+        
+        # Define VFAT3 Configuration
+        self.confVFAT3s = self.parentOH.parentAMC.lib.configureVFAT3s
+        self.confVFAT3s.argTypes = [ c_uint, c_uint ]
+        self.confVFAT3s.restype = c_uint
+
+        # Turn off calpulses
+        self.stopCalPulses2AllChannels = self.parentOH.parentAMC.lib.stopCalPulse2AllChannels
+        self.stopCalPulses2AllChannels.argTypes = [ c_uint, c_uint, c_uint, c_uint ]
+        self.stopCalPulses2AllChannels.restype = c_uint
+
+        # Write all channel regs
+        self.setChannelRegistersVFAT3 = self.parentOH.parentAMC.lib.setChannelRegistersVFAT3
+        self.setChannelRegistersVFAT3.argTypes = [ c_uint, c_uint, POINTER(c_uint32), POINTER(c_uint32), POINTER(c_uint32), POINTER(c_uint32), POINTER(c_uint32), POINTER(c_uint32) ]
+        self.setChannelRegistersVFAT3.restype = c_uint
+
+        # Set default parameters
+        self.paramsDefVals = {}
+        if self.parentOH.parentAMC.fwVersion < 3:
+            self.paramsDefVals = {
+                "ContReg1":    0x00,
+                "ContReg2":    0x30,
+                "ContReg3":    0x00,
+                "Latency":      169,
+                "IPreampIn":    144,
+                "IPreampFeed":   69,
+                "IPreampOut":   129,
+                "IShaper":      130,
+                "IShaperFeed":   87,
+                "IComp":        101,
+                "VCal":           0,
+                # ["VThreshold1":   25,
+                "VThreshold2": 0x00,
+                "CalPhase":    0x00
+                }
+
+        return
+    
+    def biasAllVFATs(self, mask=0x0, enable=False):
+        # HW Dependent Configuration
+        if self.parentOH.parentAMC.fwVersion > 2:
+            # Baseline config
+            self.confVFAT3s(self.parentOH.link,mask)
+
+            # Run mode
+            if(enable):
+                #self.parentOH.parentAMC.writeRegister("GEM_AMC.GEM_SYSTEM.VFAT3.VFAT3_RUN_MODE",0x1) #Doesn't seem to work
+                self.writeAllVFATs("CFG_RUN",0x1,mask)
+            else:
+                #self.parentOH.parentAMC.writeRegister("GEM_AMC.GEM_SYSTEM.VFAT3.VFAT3_RUN_MODE",0x0) #Doesn't seem to work
+                self.writeAllVFATs("CFG_RUN",0x0,mask)
+                pass
+        else:
+            # Run Mode
+            if (enable):
+                self.writeAllVFATs("ContReg0",    0x37, mask=mask)
+            else:
+                #what about leaving any other settings?
+                #not now, want a reproducible routine
+                self.writeAllVFATs("ContReg0",    0x36, mask=mask)
+                pass
+        
+        # User specified values - rely on the user to load self.paramsDefVals
+        for key in self.paramsDefVals.keys():
+            self.writeAllVFATs(key,self.paramsDefVals[key],mask)
+
+        return
+
+    def readAllVFATs(self, reg, mask=0x0):
+        vfatVals = self.parentOH.broadcastRead(reg,mask)
+        if vfatVals:
+            msg = "Link %i: readAllVFATs"%(self.parentOH.link)
+            for i,val in enumerate(vfatVals):
+                msg+= "%d: value = 0x%08x\n"%(i,val)
+                pass
+            self.vfatlogger.debug(colormsg(msg,logging.DEBUG))
+            pass
+        return vfatVals
+
+    def readVFAT(self, chip, reg, debug=False):
+        baseNode = ""
+        if self.parentOH.parentAMC.fwVersion > 2:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
+        else:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
+            pass
+        vfatVal = self.parentOH.parentAMC.readRegister("%s.%s"%(baseNode,reg))
+        # do check on status
+        if ((vfatVal >> 26) & 0x1) :
+            msg = "error on VFAT transaction (chip %d, %s)"%(chip, reg)
+            #gemlogger.debug(msg)
+            return -1
+        elif ((vfatVal >> 25) & 0x0):
+            msg = "invalid VFAT transaction (chip %d, %s)"%(chip, reg)
+            #gemlogger.debug(msg)
+            return -1
+        elif ((vfatVal >> 24) & 0x0):
+            msg = "wrong type of VFAT transaction (chip %d, %s)"%(chip, reg)
+            #gemlogger.debug(msg)
+            return -1
+        else:
+            return vfatVal
+
+    def setChannelRegister(self, chip, chan, mask=0x0, pulse=0x0, trimARM=0x0, trimARMPol=0x0, trimZCC=0x0, trimZCCPol=0x0, debug=False):
+        """
+        chip - VFAT to write
+        chan - channel on vfat
+        mask - channel mask
+        pulse - cal pulse enabled
+        trimARM - v2b (v3) electronics trimDAC (arm comparator trim)
+        trimARMPol - v3 electroncis only, polarity of the trimDAC for the arming comparator
+        trimZCC - v3 electronics only, zero crossing comparator trim
+        trimZCCPol - as trimARMPol but for the zero crossing comparator
+        """
+
+        # Invalid channel check
+        if (chan not in range(0,128)):
+            print "Invalid VFAT channel specified %d"%(chan)
+            return
+
+        # Write registers
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.ARM_TRIM_AMPLITUDE"%(chan), trimARM)
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.ARM_TRIM_POLARITY"%(chan), trimARMPol)
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.CALPULSE_ENABLE"%(chan), pulse)
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.MASK"%(chan), mask)
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.ZCC_TRIM_AMPLITUDE"%(chan), trimZCC)
+            self.writeVFAT(chip, "VFAT_CHANNELS.CHANNEL%d.ZCC_TRIM_POLARITY"%(chan), trimZCCPol)
+        else:
+            chanReg = ((pulse&0x1) << 6)|((mask&0x1) << 5)|(trimARM&0x1f)
+            self.writeVFAT(chip, "VFATChannels.ChanReg%d"%(chan),chanReg)
+        return
+
+    def setSpecificChannelAllRegisters(self, chan, chMask=0x0, pulse=0x0, trimARM=0x0, trimARMPol=0x0, trimZCC=0x0, trimZCCPol=0x0, vfatMask=0x0, debug=False):
+        for vfat in range(0,self.parentOH.nVFATs):
+            if (vfatMask >> vfat) & 0x1: continue
+            self.setChannelRegister(vfat, chan, chMask, pulse, trimARM, trimARMPol, trimZCC, trimZCCPol, debug)
+        return
+
+    def setAllChannelRegisters(self, chMask=None, pulse=None, trimARM=None, trimARMPol=None, trimZCC=None, trimZCCPol=None, vfatMask=0x0, debug=False):
+        """
+        Sets all channel registers for all VFAT3s on the detector
+
+        chMask - array with size 3072, storing channel MASK values, index goes as: idx = vfatN*128 + chan
+        pulse - as chMask but for CALPULSE_ENABLE values
+        trimARM - as chMask but for ARM_TRIM_AMPLITUDE values
+        trimARMPol - as chMask but for ARM_TRIM_POLARITY values
+        trimZCC - as chMask but for ZCC_TRIM_AMPLITUDE values
+        trimZCCPol - as chMask but for ZCC_TRIM_POLARITY values
+        vfatMask - 24 bit number indicating vfats to mask (n^th bit==1 means n^th VFAT ignored)
+        debug - print debug information
+        """
+
+        if chMask is None:
+            chMask = (c_uint32 * 3072)()
+            pass
+        if pulse is None:
+            pulse = (c_uint32 * 3072)()
+            pass
+        if trimARM is None:
+            trimARM = (c_uint32 * 3072)()
+            pass
+        if trimARMPol is None:
+            trimARMPol = (c_uint32 * 3072)()
+            pass
+        if trimZCC is None:
+            trimZCC = (c_uint32 * 3072)()
+            pass
+        if trimZCCPol is None:
+            trimZCCPol = (c_uint32 * 3072)()
+            pass
+
+        return self.setChannelRegistersVFAT3(self.parentOH.link, vfatMask, pulse, chMask, trimARM, trimARMPol, trimZCC, trimZCCPol)
+
+    def setDebug(self, debug):
+        self.debug = debug
+        self.parentOH.setDebug(debug)
+        return
+    
+    def setRunModeAll(self, mask=0x0, enable=True, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            if (enable):
+                self.writeAllVFATs("CFG_RUN",0x1,mask)
+            else:
+                self.writeAllVFATs("CFG_RUN",0x0,mask)
+                pass
+        else:
+            if (enable):
+                self.writeAllVFATs("ContReg0", 0x37, mask)
+            else:
+                self.writeAllVFATs("ContReg0", 0x36, mask)
+                pass
+        return
+
+    def stopCalPulses(self, mask=0x0, chanMin=0, chanMax=128):
+        """
+        Turns the cal pulse off for [chanMin, chanMax] for all vfats not in mask
+        v2b electronics only
+        """
+
+        return self.stopCalPulses2AllChannels(self.parentOH.link, mask, chanMin, chanMax)
+
+    def setVFATCalHeight(self, chip, height, currentPulse=True, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            if currentPulse: #Current pulse, high CFG_CAL_DAC is a high injected charge amount
+                self.writeVFATRegisters(chip,{"CFG_CAL_DAC": (height)})
+            else: # Voltage pulse, low CFG_CAL_DAC is a high injected charge amount
+                self.writeVFATRegisters(chip,{"CFG_CAL_DAC": (256 - height)})
+        else:
+            self.writeVFATRegisters(chip,{"VCal": height})
+        return
+
+    def setVFATCalHeightAll(self, mask=0x0, height=256, currentPulse=True, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            if currentPulse: #Current pulse, high CFG_CAL_DAC is a high injected charge amount
+                self.writeAllVFATs("CFG_CAL_DAC", (height), mask)
+            else: # Voltage pulse, low CFG_CAL_DAC is a high injected charge amount
+                self.writeAllVFATs("CFG_CAL_DAC", (256 - height), mask)
+        else:
+            self.writeAllVFATs("VCal", height, mask)
+        return
+
+    def setVFATCalPhase(self, chip, phase, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeVFATRegisters(chip,{"CFG_CAL_PHI": phase})
+        else:
+            self.writeVFATRegisters(chip,{"CalPhase": phase})
+            pass
+        return
+
+    def setVFATCalPhaseAll(self, mask=0x0, phase=0, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeAllVFATs("CFG_CAL_PHI", phase, mask)
+        else:
+            self.writeAllVFATs("CalPhase", phase, mask)
+            pass
+        return
+    
+    def setVFATLatency(self, chip, lat, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeVFATRegisters(chip,{"CFG_LATENCY": lat})
+        else:
+            self.writeVFATRegisters(chip,{"Latency": lat})
+            pass
+        return
+
+    def setVFATLatencyAll(self, mask=0x0, lat=0, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeAllVFATs("CFG_LATENCY",lat,mask)
+        else:
+            self.writeAllVFATs("Latency",lat,mask)
+            pass
+
+        return
+    
+    def setVFATMSPLAll(self, mask=0x0, mspl=4, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeAllVFATs("CFG_PULSE_STRETCH",mspl,mask)
+        else:
+            self.writeAllVFATs("ContReg2",((mspl-1)<<4),mask)
+            pass
+
+        return
+
+    def setVFATThreshold(self, chip, vt1, vt2=0, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeVFATRegisters(chip,{"CFG_THR_ARM_DAC": vt1})
+        else:
+            self.writeVFATRegisters(chip,{"VThreshold1": vt1,"VThreshold2": vt2})
+            pass
+        return
+
+    def setVFATThresholdAll(self, mask=0x0, vt1=100, vt2=0, debug=False):
+        if self.parentOH.parentAMC.fwVersion > 2:
+            self.writeAllVFATs("CFG_THR_ARM_DAC",vt1,mask)
+        else:
+            self.writeAllVFATs("VThreshold1",vt1,mask)
+            self.writeAllVFATs("VThreshold2",vt2,mask)
+            pass
+
+        return
+    
+    def writeAllVFATs(self, reg, value, mask=0x0):
+        return self.parentOH.broadcastWrite(reg,value,mask)
+    
+    def writeVFAT(self, chip, reg, value, debug=False):
+        baseNode = ""
+        if self.parentOH.parentAMC.fwVersion > 2:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
+        else:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
+            pass
+        self.parentOH.parentAMC.writeRegister("%s.%s"%(baseNode,reg), value)
+
+    def writeVFATRegisters(self, chip, regs_with_values, debug=False):
+        baseNode = ""
+        if self.parentOH.parentAMC.fwVersion > 2:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
+        else:
+            baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
+            pass
+        registers = {}
+        for reg in regs_with_values.keys():
+            registers["%s.%s"%(baseNode,reg)] = regs_with_values[reg]
+            pass
+        self.parentOH.parentAMC.writeRegisterList(registers)
