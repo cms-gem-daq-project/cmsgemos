b4a7d66514ef7507b6a179f60425269f25eabf32
Brian Dorney <brian.l.dorney@cern.ch>
2018-07-10 20:53:13 +0200

    improvements from codacy

--- gempython/tools/vfat_user_functions_xhal.py
+++ gempython/tools/vfat_user_functions_xhal.py
@@ -1,4 +1,4 @@
-import os, sys, time
+import os, sys
 
 from gempython.tools.optohybrid_user_functions_xhal import *
 from gempython.utils.gemlogger import colormsg
@@ -12,7 +12,7 @@ class HwVFAT(object):
         """
         # Debug flag
         self.debug = debug
-         
+
         # Logger
         self.vfatlogger = logging.getLogger(__name__)
 
@@ -75,7 +75,6 @@ class HwVFAT(object):
                 #what about leaving any other settings?
                 #not now, want a reproducible routine
                 self.writeAllVFATs("ContReg0",    0x36, mask=mask)
-                pass
         
         # User specified values - rely on the user to load self.paramsDefVals
         for key in self.paramsDefVals.keys():
@@ -89,9 +88,7 @@ class HwVFAT(object):
             msg = "Link %i: readAllVFATs"%(self.parentOH.link)
             for i,val in enumerate(vfatVals):
                 msg+= "%d: value = 0x%08x\n"%(i,val)
-                pass
             self.vfatlogger.debug(colormsg(msg,logging.DEBUG))
-            pass
         return vfatVals
 
     def readVFAT(self, chip, reg, debug=False):
@@ -100,7 +97,6 @@ class HwVFAT(object):
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
         else:
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
-            pass
         vfatVal = self.parentOH.parentAMC.readRegister("%s.%s"%(baseNode,reg))
         # do check on status
         if ((vfatVal >> 26) & 0x1) :
@@ -170,22 +166,16 @@ class HwVFAT(object):
 
         if chMask is None:
             chMask = (c_uint32 * 3072)()
-            pass
         if pulse is None:
             pulse = (c_uint32 * 3072)()
-            pass
         if trimARM is None:
             trimARM = (c_uint32 * 3072)()
-            pass
         if trimARMPol is None:
             trimARMPol = (c_uint32 * 3072)()
-            pass
         if trimZCC is None:
             trimZCC = (c_uint32 * 3072)()
-            pass
         if trimZCCPol is None:
             trimZCCPol = (c_uint32 * 3072)()
-            pass
 
         return self.setChannelRegistersVFAT3(self.parentOH.link, vfatMask, pulse, chMask, trimARM, trimARMPol, trimZCC, trimZCCPol)
 
@@ -200,13 +190,11 @@ class HwVFAT(object):
                 self.writeAllVFATs("CFG_RUN",0x1,mask)
             else:
                 self.writeAllVFATs("CFG_RUN",0x0,mask)
-                pass
         else:
             if (enable):
                 self.writeAllVFATs("ContReg0", 0x37, mask)
             else:
                 self.writeAllVFATs("ContReg0", 0x36, mask)
-                pass
         return
 
     def stopCalPulses(self, mask=0x0, chanMin=0, chanMax=128):
@@ -242,7 +230,6 @@ class HwVFAT(object):
             self.writeVFATRegisters(chip,{"CFG_CAL_PHI": phase})
         else:
             self.writeVFATRegisters(chip,{"CalPhase": phase})
-            pass
         return
 
     def setVFATCalPhaseAll(self, mask=0x0, phase=0, debug=False):
@@ -250,7 +237,6 @@ class HwVFAT(object):
             self.writeAllVFATs("CFG_CAL_PHI", phase, mask)
         else:
             self.writeAllVFATs("CalPhase", phase, mask)
-            pass
         return
     
     def setVFATLatency(self, chip, lat, debug=False):
@@ -258,7 +244,6 @@ class HwVFAT(object):
             self.writeVFATRegisters(chip,{"CFG_LATENCY": lat})
         else:
             self.writeVFATRegisters(chip,{"Latency": lat})
-            pass
         return
 
     def setVFATLatencyAll(self, mask=0x0, lat=0, debug=False):
@@ -266,7 +251,6 @@ class HwVFAT(object):
             self.writeAllVFATs("CFG_LATENCY",lat,mask)
         else:
             self.writeAllVFATs("Latency",lat,mask)
-            pass
 
         return
     
@@ -275,7 +259,6 @@ class HwVFAT(object):
             self.writeAllVFATs("CFG_PULSE_STRETCH",mspl,mask)
         else:
             self.writeAllVFATs("ContReg2",((mspl-1)<<4),mask)
-            pass
 
         return
 
@@ -284,7 +267,6 @@ class HwVFAT(object):
             self.writeVFATRegisters(chip,{"CFG_THR_ARM_DAC": vt1})
         else:
             self.writeVFATRegisters(chip,{"VThreshold1": vt1,"VThreshold2": vt2})
-            pass
         return
 
     def setVFATThresholdAll(self, mask=0x0, vt1=100, vt2=0, debug=False):
@@ -293,7 +275,6 @@ class HwVFAT(object):
         else:
             self.writeAllVFATs("VThreshold1",vt1,mask)
             self.writeAllVFATs("VThreshold2",vt2,mask)
-            pass
 
         return
     
@@ -306,7 +287,6 @@ class HwVFAT(object):
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
         else:
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
-            pass
         self.parentOH.parentAMC.writeRegister("%s.%s"%(baseNode,reg), value)
 
     def writeVFATRegisters(self, chip, regs_with_values, debug=False):
@@ -315,9 +295,7 @@ class HwVFAT(object):
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFAT%d"%(self.parentOH.link,chip)
         else:
             baseNode = "GEM_AMC.OH.OH%d.GEB.VFATS.VFAT%d"%(self.parentOH.link,chip)
-            pass
         registers = {}
         for reg in regs_with_values.keys():
             registers["%s.%s"%(baseNode,reg)] = regs_with_values[reg]
-            pass
         self.parentOH.parentAMC.writeRegisterList(registers)
