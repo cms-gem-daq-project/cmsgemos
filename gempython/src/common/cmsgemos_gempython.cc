206bf3d18dd0a9faf07a2013c8268f5d6e27c647
Jared Sturdy <sturdy@cern.ch>
2018-10-22 16:39:45 +0200

    update gempython boost bindings

--- gempython/src/common/cmsgemos_gempython.cc
+++ gempython/src/common/cmsgemos_gempython.cc
@@ -19,6 +19,8 @@
 // #include "gem/hw/ctp7/HwCTP7.h"
 #include "gem/hw/optohybrid/HwOptoHybrid.h"
 
+#include "gem/hw/utils/GEMCrateUtils.h"
+
 // gemhardware enum includes
 #include "gem/hw/GenericAMCSettingsEnums.h"
 #include "gem/hw/optohybrid/OptoHybridSettingsEnums.h"
@@ -43,13 +45,16 @@ BOOST_PYTHON_MODULE(_cmsgemos_gempython) {
 
   // GEMHwDevice class
   bpy::class_<gem::hw::GEMHwDevice, boost::noncopyable>("GEMHwDevice", bpy::no_init)
-    .def("getLoggerName", &gem::hw::GEMHwDevice::getLoggerName);
+    .def("getLoggerName", &gem::hw::GEMHwDevice::getLoggerName)
+    .def("scaConversion", &gem::hw::utils::scaConversion)
+    ;
   // bpy::class_<gem::hw::GEMHwDevice, boost::noncopyable>("GEMHwDevice", bpy::init<const std::string&, const std::string&>());
                                     // bpy::no_init)
     // .def(/*__str__*/ bpy::self_ns::str(bpy::self));
 
   bpy::class_<gem::hw::HwGenericAMC, bpy::bases<gem::hw::GEMHwDevice>, boost::noncopyable>("HwGenericAMC",
-                                                                        bpy::init<const std::string&,const std::string&>())
+                                                                                           bpy::init<const std::string&,
+                                                                                           const std::string&>())
     .def("getBoardID",          &gem::hw::HwGenericAMC::getBoardID)
     .def("getBoardIDRaw",       &gem::hw::HwGenericAMC::getBoardIDRaw)
     .def("getFirmwareVer",      &gem::hw::HwGenericAMC::getFirmwareVer)
@@ -58,15 +63,18 @@ BOOST_PYTHON_MODULE(_cmsgemos_gempython) {
     .def("getFirmwareDateRaw",  &gem::hw::HwGenericAMC::getFirmwareDateRaw)
     .def("getUserFirmware",     &gem::hw::HwGenericAMC::getUserFirmware)
     .def("getUserFirmwareDate", &gem::hw::HwGenericAMC::getUserFirmwareDate)
+    .def("scaConversion",       &gem::hw::utils::scaConversion)
     // .def(/*__str__*/ bpy::self_ns::str(bpy::self))
     ;
 
   bpy::class_<gem::hw::optohybrid::HwOptoHybrid, bpy::bases<gem::hw::GEMHwDevice>, boost::noncopyable>("HwOptoHybrid",
-                                                                                    bpy::init<const std::string&,const std::string&>())
+                                                                                                       bpy::init<const std::string&,
+                                                                                                       const std::string&>())
     .def("getFirmwareVersion",       &gem::hw::optohybrid::HwOptoHybrid::getFirmwareVersion)
     .def("getFirmwareVersionString", &gem::hw::optohybrid::HwOptoHybrid::getFirmwareVersionString)
     .def("getFirmwareDate",          &gem::hw::optohybrid::HwOptoHybrid::getFirmwareDate)
     .def("getFirmwareDateString",    &gem::hw::optohybrid::HwOptoHybrid::getFirmwareDateString)
+    .def("scaConversion",            &gem::hw::utils::scaConversion)
     // .def(/*__str__*/ bpy::self_ns::str(bpy::self))
     ;
 }
