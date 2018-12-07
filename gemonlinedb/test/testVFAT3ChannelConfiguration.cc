d149d07118d7a40b019a216109dd997bd6ce7d44
Louis Moureaux <lmoureau@ulb.ac.be>
2018-12-07 14:12:38 +0100

    Add VFAT3ChannelConfiguration and related tests
    This new class contains channel registers for a VFAT3. Register contents
    are stored in such a way that accessing individual fields is easy.

---
+++ gemonlinedb/test/testVFAT3ChannelConfiguration.cc
@@ -0,0 +1,54 @@
+#include "gem/onlinedb/VFAT3ChannelConfiguration.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE XMLSerializationData
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <iomanip>
+
+/* Needed to make the linker happy. */
+#include <xdaq/version.h>
+config::PackageInfo xdaq::getPackageInfo()
+{
+    return config::PackageInfo("", "", "", "", "", "", "", "");
+}
+
+using namespace gem::onlinedb;
+
+BOOST_AUTO_TEST_SUITE(TestVFAT3ChannelConfiguration)
+
+BOOST_AUTO_TEST_CASE(ChannelRegisterValue)
+{
+    VFAT3ChannelConfiguration::ChannelRegisterValue value;
+
+    value.registerValue = 0xFFFF;
+    BOOST_CHECK(value.fields.cal == true);
+    BOOST_CHECK(value.fields.mask == true);
+
+    value.registerValue = 0;
+    value.fields.cal = true;
+    BOOST_CHECK(value.registerValue == (1 << 15));
+
+    value.registerValue = 0;
+    value.fields.armDacCorrectionPolarity = 1;
+    BOOST_CHECK(value.registerValue == (1 << 6));
+
+    value.registerValue = 0;
+    value.fields.zccDacCorrectionPolarity = 1;
+    BOOST_CHECK(value.registerValue == (1 << 13));
+
+    value.registerValue = 0;
+    value.fields.zccDacCorrectionAmplitude = 0b101010;
+    BOOST_CHECK(value.registerValue == (0b101010 << 7));
+}
+
+BOOST_AUTO_TEST_CASE(SerializationData)
+{
+    VFAT3ChannelConfiguration config;
+    auto data = config.getRegisterData();
+    BOOST_CHECK(data.count("CHANNEL_REG_15") == 1);
+    config.readRegisterData(data);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
