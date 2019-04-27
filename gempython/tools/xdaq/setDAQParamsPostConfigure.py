c83a5e3bf10c2ecc9baeae56b6102ca8bdd00fb0
Brian Dorney <brian.l.dorney@cern.ch>
2019-04-27 11:32:19 +0200

    now possible to configure multiple AMC's with single call

--- gempython/tools/xdaq/setDAQParamsPostConfigure.py
+++ gempython/tools/xdaq/setDAQParamsPostConfigure.py
@@ -1,32 +1,41 @@
 #!/bin/env python
 
 from gempython.tools.amc_user_functions_uhal import *
+from gempython.utils.gemlogger import printGreen, printRed, printYellow
 
 def setDAQParams(args):
     from gempython.utils.gracefulKiller import GracefulKiller
     killer = GracefulKiller()
 
-    # Make the AMC Object w/uhal
-    global amc
-    amc = getAMCObject(args.slot,args.shelf,args.d)
+    for slot in range(1,13):
+        # Skip unmasked slots
+        if (not ((args.slotMask >> slot) & 0x1)):
+            continue
 
-    # Set the input enable mask
-    amc.getNode("GEM_AMC.DAQ.CONTROL.INPUT_ENABLE_MASK").write(args.ohMask)
-    amc.dispatch()
+        # Make the AMC Object w/uhal
+        global amc
+        amc = getAMCObject(args.slot,args.shelf,args.d)
 
-    # Ensure that L1A's are enabled to this AMC
-    enableL1A(amc)
+        # Get ohMask for this slot
+        from gempython.tools.amc_user_functions_xhal import HwAMC
+        cardName = "gem-shelf%02d-amc%02d"%(args.shelf,args.slot)
+        amcRPC = HwAMC(cardName,debug=args.d)
+        ohMask = amcRPC.getOHMask(raiseIfNoOHs=False)
 
-    # Ensure the TTC Generator is Disabled (not sure if this is in uhal addr table...)
-    from gempython.tools.amc_user_functions_xhal import HwAMC
-    cardName = "gem-shelf%02d-amc%02d"%(args.shelf,args.slot)
-    amcRPC = HwAMC(cardName,debug=args.d)
-    amcRPC.writeRegister("GEM_AMC.TTC.GENERATOR.ENABLE",0x0,args.d)
+        # Set the input enable mask
+        amc.getNode("GEM_AMC.DAQ.CONTROL.INPUT_ENABLE_MASK").write(ohMask)
+        amc.dispatch()
+
+        # Ensure that L1A's are enabled to this AMC
+        enableL1A(amc)
+
+        # Ensure the TTC Generator is Disabled (not sure if this is in uhal addr table...)
+        amcRPC.writeRegister("GEM_AMC.TTC.GENERATOR.ENABLE",0x0,args.d)
+        pass
 
     from time import sleep
-    from gempython.utils.gemlogger import printGreen, printRed
-    printRed("I will now wait 60 seconds; you must navigate to the RCMS Page and Press Start")
-    sleep(60)
+    printYellow("I will now wait 30 seconds; you must navigate to the RCMS Page and Press Start")
+    sleep(30)
     amc.getNode("GEM_AMC.DAQ.CONTROL.ZERO_SUPPRESSION_EN").write(0x0)
     amc.dispatch()
     printGreen("Zero Suppression has been correctly disabled; happy data taking!!")
@@ -39,10 +48,9 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("ohMask",       help="ohMask to apply, a 1 in the n^th bit indicates the n^th OH should be considered", type=parseInt)
-    parser.add_argument("-s","--slot", help="Slot number",         type=int)
-    parser.add_argument("--shelf",     help="uTCA shelf number",   type=int)
-    parser.add_argument("-d",          help="debug",    action='store_true')
+    parser.add_argument("-s","--slotMask", help="Slot mask to apply, a 1 in the n^th bit indicates the n^th slot should be considered", type=parseInt)
+    parser.add_argument("--shelf", help="uTCA shelf number", type=int)
+    parser.add_argument("-d", help="debug", action='store_true')
 
     args = parser.parse_args()
 
@@ -55,8 +63,8 @@ if __name__ == '__main__':
 
     if args.d:
         print("Running checks")
-    if args.slot < 0 or args.slot > 12:
-        print("Invalid slot specified {}".format(args.slot))
+    if args.slotMask < 0x0 or args.slotMask > 0xfff:
+        printRed("Invalid slotMask specified 0x{0:x}\nslotMask must be in [0x0,0xfff]".format(args.slotMask))
         exit(os.EX_USAGE)
 
     if args.d:
